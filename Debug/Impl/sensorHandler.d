2cc9a1de4e19288565611ea6bf25223b9cbb566b
GluhaiaMuha <57876096+GluhaiaMuha@users.noreply.github.com>
2023-05-09 09:20:15 +0200

    Revert "Added missing files. Deleted all unnecessary files that are ignored."
    This reverts commit 88825f7d2c2f39770b14885e33e75fdba1f02766.

---
+++ Debug/Impl/sensorHandler.d
@@ -0,0 +1,85 @@
+Impl/sensorHandler.d Impl/sensorHandler.o: ../Impl/sensorHandler.c \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ ../FreeRTOS/src/ATMEGA_FreeRTOS.h ../FreeRTOS/src/FreeRTOSConfig.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ D:\MicroChipStudio\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom2560.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ ../FreeRTOS/src/projdefs.h ../FreeRTOS/src/portable.h \
+ ../FreeRTOS/src/portmacro.h ../FreeRTOS/src/mpu_wrappers.h \
+ ../FreeRTOS/src/FreeRTOSVariant.h \
+ d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\wdt.h \
+ ../FreeRTOS/src/ATMEGA_FreeRTOS.h ../FreeRTOS/src/task.h \
+ ../FreeRTOS/src/list.h ../FreeRTOS/src/task.h \
+ ../Impl/../Headers/dataHandler.h ../Impl/../Headers/sensorHandler.h \
+ ../Impl/../Headers/temperature.h
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+../FreeRTOS/src/ATMEGA_FreeRTOS.h:
+
+../FreeRTOS/src/FreeRTOSConfig.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+D:\MicroChipStudio\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom2560.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+../FreeRTOS/src/projdefs.h:
+
+../FreeRTOS/src/portable.h:
+
+../FreeRTOS/src/portmacro.h:
+
+../FreeRTOS/src/mpu_wrappers.h:
+
+../FreeRTOS/src/FreeRTOSVariant.h:
+
+d:\microchipstudio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\wdt.h:
+
+../FreeRTOS/src/ATMEGA_FreeRTOS.h:
+
+../FreeRTOS/src/task.h:
+
+../FreeRTOS/src/list.h:
+
+../FreeRTOS/src/task.h:
+
+../Impl/../Headers/dataHandler.h:
+
+../Impl/../Headers/sensorHandler.h:
+
+../Impl/../Headers/temperature.h:
