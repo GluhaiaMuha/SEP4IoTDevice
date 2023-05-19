8f5ce20cfd92036bd0190c8defbc50b2ca6859ad
foxide123 <kuba.kolando.02.01@gmail.com>
2023-05-19 18:09:35 +0200

    lora_test setup

---
+++ test/src/lora_test.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+#include "fff/fff.h"
+#include "FreeRTOS_defs/FreeRTOS_FFF_MocksDeclaration.h"
+
+extern "C"
+{
+    #include <lora_driver.h>
+    #include <status_leds.h>
+    #include <LoRaWANHandler.h>
+}
+
+FAKE_VOID_FUNC(lora_driver_resetRn2483, uint8_t);
+FAKE_VOID_FUNC(lora_driver_flushBuffers);
+FAKE_VOID_FUNC(status_leds_slowBlink, status_leds_t);
+FAKE_VOID_FUNC(status_leds_longPuls, status_leds_t);
+FAKE_VOID_FUNC(status_leds_ledOn, status_leds_t);
+FAKE_VOID_FUNC(status_leds_ledOff, status_leds_t);
+FAKE_VOID_FUNC(status_leds_fastBlink, status_leds_t);
+FAKE_VOID_FUNC(status_leds_shortPuls, status_leds_t);
+
+FAKE_VALUE_FUNC(char *, lora_driver_mapReturnCodeToText, lora_driver_returnCode_t);
+FAKE_VALUE_FUNC(lora_driver_returnCode_t, lora_driver_join, lora_driver_joinMode_t);
+FAKE_VALUE_FUNC(lora_driver_returnCode_t, lora_driver_sendUploadMessage, bool, lora_driver_payload_t *);
+FAKE_VALUE_FUNC(lora_driver_returnCode_t, lora_driver_getRn2483Hweui, char*);
+
+class Lora_test : public ::testing::Test
+{
+protected:
+	void SetUp() override
+	{
+        RESET_FAKE(lora_driver_mapReturnCodeToText);
+        RESET_FAKE(lora_driver_getRn2483Hweui);
+        RESET_FAKE(lora_driver_join);
+        RESET_FAKE(lora_driver_resetRn2483);
+        RESET_FAKE(lora_driver_flushBuffers);
+        RESET_FAKE(lora_driver_sendUploadMessage);
+
+        RESET_FAKE(status_leds_slowBlink);
+        RESET_FAKE(status_leds_longPuls);
+        RESET_FAKE(status_leds_ledOn);
+        RESET_FAKE(status_leds_ledOff);
+        RESET_FAKE(status_leds_fastBlink);
+        RESET_FAKE(status_leds_shortPuls);
+        RESET_FAKE(xTaskCreate);
+        RESET_FAKE(vTaskDelay);
+        RESET_FAKE(xTaskGetTickCount);
+        RESET_FAKE(xTaskDelayUntil);
+		FFF_RESET_HISTORY();
+	}
+	void TearDown() override
+	{}
+};
+
+
+
+
+//initialise
+  //xTaskCreate
+//setup
+
+//handler_task
