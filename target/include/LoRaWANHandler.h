10817419a76b3b61797a8e30bc0bcb849b86226c
foxide123 <kuba.kolando.02.01@gmail.com>
2023-05-22 01:15:04 +0200

    setup test and deleting static from _lora_setup

--- target/include/LoRaWANHandler.h
+++ target/include/LoRaWANHandler.h
@@ -2,5 +2,5 @@
 
 void lora_handler_task( void *pvParameters );
 void lora_handler_initialise(UBaseType_t lora_handler_task_priority);
-static void _lora_setup(void);
+void _lora_setup();
 void lora_handler_task( void *pvParameters );
