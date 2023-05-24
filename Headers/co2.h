e9d483bb52710ac90ab7347b923bb0bd849c6302
GluhaiaMuha <57876096+GluhaiaMuha@users.noreply.github.com>
2023-05-24 23:43:30 +0200

    Changes in the CO2.h file. Nothing important.

--- Headers/co2.h
+++ Headers/co2.h
@@ -5,11 +5,11 @@
  * Created: 5/9/2023 9:56:04 PM
  *  Author: Marius, Mihail
  */ 
-
+#pragma once
 #include <stdint.h>
 
-void CO2_create();
-int16_t CO2_getValue();
-void CO2_task();
-uint16_t CO2_sensor_get_last_reading();
+void co2_sensor_create();
+void co2_sensor_measure();
+uint16_t co2_sensor_get_last_reading();
+void co2_task();
 
