756792d15bac12afc59f4fe1e579cca90a80e8a2
Bogdan0002 <fellyx1@gmail.com>
2023-05-24 13:20:20 +0200

    co2 sensor get last reading test

--- test/src/co2_test.cpp
+++ test/src/co2_test.cpp
@@ -51,3 +51,15 @@ TEST_F(CO2_test, Should_call_mh_z19_takeMeassuring_when_co2_sensor_measure_is_ca
     EXPECT_EQ(mh_z19_takeMeassuring_fake.call_count, 1);
 }
 
+
+TEST_F(CO2_test, Should_return_last_reading_when_co2_sensor_get_last_reading_is_called)
+{
+    // Arrange
+    int8_t lastCo2Recorded = 123;
+
+    // Act
+    uint16_t result = co2_sensor_get_last_reading();
+
+    // Assert
+    EXPECT_EQ(result, 123);
+}
