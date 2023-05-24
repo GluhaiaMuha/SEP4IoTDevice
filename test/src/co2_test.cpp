6c577c9e99e0a8a677b2ee613ec0a42f52054ace
Bogdan0002 <fellyx1@gmail.com>
2023-05-24 13:17:15 +0200

    sensor init test

--- test/src/co2_test.cpp
+++ test/src/co2_test.cpp
@@ -1,1 +1,41 @@
+#include <gtest/gtest.h>
+#include "fff/fff.h"
+
+extern "C"
+{
+	#include <co2.h>
+	#include <mh_z19.h>
+}
+
+FAKE_VOID_FUNC(mh_z19_initialise, serial_comPort_t);
+FAKE_VALUE_FUNC(mh_z19_returnCode_t, mh_z19_takeMeassuring);
+
+// Create Test fixture and Reset all Mocks before each test
+class CO2_test : public ::testing::Test
+{
+protected:
+	void SetUp() override
+	{
+		RESET_FAKE(mh_z19_initialise);
+		RESET_FAKE(mh_z19_takeMeassuring);
+		FFF_RESET_HISTORY();
+	}
+	void TearDown() override
+	{}
+};
+
+TEST_F(CO2_test, Should_call_mh_z19_initialise_when_co2_sensor_create_is_called)
+{
+	// Arrange
+//	mh_z19_initialise_fake.custom_fake = [](int arg) {
+//		// Custom behavior for the mock function
+//		EXPECT_EQ(arg, ser_USART3); // Check the argument passed to the mock function
+//	};
+
+	// Act
+	co2_sensor_create();
+
+	// Assert
+	EXPECT_EQ(mh_z19_initialise_fake.call_count, 1);
+} 
 
