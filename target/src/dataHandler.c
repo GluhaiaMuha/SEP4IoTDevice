ec7d218f5654da2867c4dc38701f103df9308a4b
foxide123 <kuba.kolando.02.01@gmail.com>
2023-05-25 15:59:05 +0200

    Adding dataHandler and sensorHandler

---
+++ target/src/dataHandler.c
@@ -0,0 +1,63 @@
+
+/*
+ * dataHandler.c
+ *
+ * Created: 5/1/2023 9:42:59 PM
+ *  Author: Marius
+ */ 
+
+#include <stdio.h>
+#include <stdint.h>
+#include <ATMEGA_FreeRTOS.h>
+#include <semphr.h>
+
+static int16_t dataHandlerTemperature;
+static int16_t dataHandlerHumidity;
+static int16_t dataHandlerAvgTemperature;
+static uint16_t dataHandlerCo2;
+
+
+//For Temperature
+int16_t dataHandler_getTempData()
+{
+	return dataHandlerTemperature;
+}
+
+
+void dataHandler_setTemperature(int16_t sensorTemperature)
+{
+	dataHandlerTemperature = sensorTemperature;
+}
+
+
+void dataHandler_setAvgTemperature(int16_t sensorAvgTemperature)
+{
+	dataHandlerAvgTemperature = sensorAvgTemperature;
+}
+
+
+int16_t dataHandler_getAvgTempeature()
+{
+	return dataHandlerAvgTemperature;
+}
+
+//For Humidity
+void dataHandler_setHumidity(int16_t sensorHumidity)
+{
+	dataHandlerHumidity = sensorHumidity;
+}
+
+int16_t dataHandler_getHumData()
+{
+	return dataHandlerHumidity;
+}
+
+void dataHandler_setCo2(uint16_t sensorCo2)
+{
+	dataHandlerCo2 = sensorCo2;
+}
+
+uint16_t dataHandler_getCo2Data()
+{
+	return dataHandlerCo2;
+}
---
+++ target/src/sensorHandler.c
@@ -0,0 +1,79 @@
+
+/*
+ * sensorHandler.c
+ *
+ * Created: 5/1/2023 9:57:43 PM
+ *  Author: Marius
+ */ 
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <ATMEGA_FreeRTOS.h>
+#include <task.h>
+
+#include <dataHandler.h>
+#include <sensorHandler.h>
+#include <temperature.h>
+#include <co2.h>
+
+static int16_t lastTempRecorded;
+static int16_t lastHumidityRecorded;
+static int16_t lastAvgRecorded;
+static uint16_t lastCo2Recorded;
+
+
+void sensorsHandler_createSensors()
+{
+	temperature_create();
+	
+	xTaskCreate(
+	temperature_task
+	,  "temperatureTask"  // A name just for humans
+	,  configMINIMAL_STACK_SIZE  // This stack size can be checked & adjusted by reading the Stack Highwater
+	,  NULL
+	,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+	,  NULL );
+	
+	co2_sensor_create();
+	xTaskCreate(
+	co2_task
+	, "co2Task"
+	, configMINIMAL_STACK_SIZE
+	, NULL
+	, 2
+	,NULL);
+
+}
+
+/************************************************************************/
+/* Responsible for handling sensors                                     */
+/************************************************************************/
+
+void sensorsHandler_task(void* pvParameters)
+{
+	// Remove compiler warnings
+	(void)pvParameters;
+	
+	TickType_t xLastWakeTime;
+	xLastWakeTime = xTaskGetTickCount();
+	const TickType_t xFrequency = 30000/portTICK_PERIOD_MS; // 300000 ms == 5 mins
+	
+	for(;;)
+	{
+		printf("SensorHandler Task Started\n");
+		xTaskDelayUntil(&xLastWakeTime, xFrequency);
+		
+		lastTempRecorded = temperature_getLatestTemperature();
+		dataHandler_setTemperature(lastTempRecorded);
+		
+		lastAvgRecorded = temperature_getAvgTemperature();
+		dataHandler_setAvgTemperature(lastAvgRecorded);
+		
+		lastHumidityRecorded = humidity_getLatestHumidity();
+		dataHandler_setHumidity(lastHumidityRecorded);
+		
+
+		lastCo2Recorded = co2_sensor_get_last_reading();
+		dataHandler_setCo2(lastCo2Recorded);
+	}
+}
