0fd61b6c291f7d6d47a3af2af6dc18f4b9fb2fff
Bogdan0002 <fellyx1@gmail.com>
2023-05-27 01:19:27 +0200

    Servo_test setup

---
+++ target/src/servo.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <ATMEGA_FreeRTOS.h>
+#include <task.h>
+#include <rc_servo.h>
+
+#include <servo.h>
+#include <dataHandler.h>
+
+
+static int8_t position;
+static uint8_t servoNo = 0;
+
+static TickType_t xLastWakeTime;
+static TickType_t xFrequency;
+
+void servo_create(){
+
+	rc_servo_initialise();
+
+	printf("Initialization of servo was successful!\n");
+}
+void servo_turnOn()
+{
+	position = 100;
+	rc_servo_setPosition(servoNo, position);
+	printf("Servo is turned on!\n");
+	struct ServoLimits limits = dataHandler_getLimits();
+	printf("Limits: %d %d\n", limits.minTempLimit, limits.maxTempLimit);
+}
+void servo_turnOff()
+{
+	position = 0;
+	rc_servo_setPosition(servoNo, position);
+	printf("Servo is turned off!\n");
+}
+
+
+
+static limitResult co2Handler_compareData(int16_t temperature, int16_t minLimit, int16_t maxLimit)
+{
+	limitResult result;
+	if (temperature >= minLimit && temperature <= maxLimit)
+	{
+		result = BETWEEN;
+	}
+	else if (temperature < minLimit)
+	{
+		result = BELLOW;
+	}
+	else
+	{
+		result = ABOVE;
+	}
+	return result;
+}
+
+
+void servoHandler_Init()
+{
+	xLastWakeTime  = xTaskGetTickCount();
+	xFrequency = pdMS_TO_TICKS(150000); // = 2.5 min
+}
+
+
+void servoHandler_run(void)
+{
+	printf("Servo Task Started\n");
+	xTaskDelayUntil(&xLastWakeTime, xFrequency);
+	
+    int16_t tempData = dataHandler_getTempData();
+	struct ServoLimits limits = dataHandler_getLimits();
+
+	limitResult result = co2Handler_compareData(tempData, limits.minTempLimit, limits.maxTempLimit);
+
+	if(result == BELLOW)
+	{
+		servo_turnOff();
+	}
+	else if(result == ABOVE)
+	{
+		servo_turnOn();
+	}
+
+}
+
+void servo_createTask(void* pvParameters)
+{
+	(void)pvParameters;
+	
+	servoHandler_Init();
+
+	for(;;)
+	{
+		servoHandler_run();
+	}
+}
