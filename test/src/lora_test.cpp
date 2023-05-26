9dbaed1ae48b8bec8e8b9698029cca77ce85a8e5
foxide123 <kuba.kolando.02.01@gmail.com>
2023-05-26 14:43:21 +0200

    Uncommenting passing tests from lora_test

--- test/src/lora_test.cpp
+++ test/src/lora_test.cpp
@@ -65,7 +65,7 @@ protected:
   {
   }
 };
-/*
+
 // initialise
 TEST_F(Lora_test, Should_call_xTaskCreate_with_correct_parameter_when_lora_handler_initialise_is_called)
 {
@@ -75,6 +75,7 @@ TEST_F(Lora_test, Should_call_xTaskCreate_with_correct_parameter_when_lora_handl
 
   EXPECT_EQ(xTaskCreate_fake.call_count, 1);
 }
+
 // setup
 TEST_F(Lora_test, Should_call_lora_methods_when_lora_setup_is_called_with_LORA_ACCEPTED)
 {
@@ -95,6 +96,7 @@ TEST_F(Lora_test, Should_call_lora_methods_when_lora_setup_is_called_with_LORA_A
   EXPECT_EQ(lora_driver_join_fake.call_count, 1);
 }
 
+/*
 TEST_F(Lora_test, Should_call_lora_methods_when_lora_setup_is_called_with_LORA_ERROR)
 {
   lora_driver_join_fake.return_val = LORA_ERROR;
@@ -119,6 +121,7 @@ TEST_F(Lora_test, Should_call_lora_methods_when_lora_setup_is_called_with_LORA_E
 // handler_task
 TEST_F(Lora_test, Should_call_methods_when_lora_handler_task_is_called)
 {
+  lora_handler_task(nullptr);
   EXPECT_EQ(lora_driver_resetRn2483_fake.call_count, 2);
   EXPECT_EQ(vTaskDelay_fake.call_count, 2);
   EXPECT_EQ(lora_driver_flushBuffers_fake.call_count, 1);
